0070e338d33134f2c5d4b47f03f96540bf61f20c
Isaiah <isaiahraspet@gmail.com>
2021-07-28 16:07:44 -0700

    initial commit

---
+++ BB84GameLib/BB84GameLib/Game.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+
+namespace BB84GameLib
+{
+    public class Game
+    {
+        string GameData;
+        bool GameStarted;
+
+        ServerHandler ServerHandler;
+        Scene Scene;
+
+        //set events
+        public event EventHandler Update;
+        public event EventHandler PlayerReadied;
+        public event EventHandler GameReset;
+        public event EventHandler GameStart;
+
+        protected virtual void OnUpdate(EventArgs e)
+        {
+            object LOCK = new object();
+
+            lock (LOCK)
+            {
+                //parse the update
+                Scene.ParseUpdate(GameData);
+
+                //invoke the event, let the visualizer handle what happens with 
+                //the information
+                EventHandler handler = Update;
+                handler?.Invoke(this, e);
+            }
+        }
+        protected virtual void OnPlayerReadied(EventArgs e)
+        {
+            EventHandler handler = PlayerReadied;
+            handler.Invoke(this, e);
+        }
+        protected virtual void OnGameReset(EventArgs e)
+        {
+            GameStarted = false;
+            GameData = "";
+            EventHandler handler = GameReset;
+            handler?.Invoke(this, e);
+        }
+        protected virtual void OnGameStart(EventArgs e)
+        {
+            EventHandler handler = GameStart;
+            handler?.Invoke(this, e);
+        }
+        //end event declarations
+
+        //wait for a new update from the server
+        public void GetUpdate()
+        { 
+            while(GameData == ServerHandler.GetUpdate())
+            {
+
+                //sleep here
+            }
+            GameData = ServerHandler.GetUpdate();
+            //if the game hasn't registered start - start it
+            if (!GameStarted)
+            {
+                OnGameStart(new EventArgs());
+                GameStarted = true;
+            }
+            OnUpdate(new EventArgs());
+        }
+
+        public void Ready()
+        {
+            //post ready to server
+            ServerHandler.Ready();
+
+            //begin update loop
+            GetUpdate();
+        }
+
+        //post updated game to server
+        public void PostUpdate()
+        {
+            //post gamedata to server
+            ServerHandler.PostUpdate(GameData);
+            //continue update loop
+            GetUpdate();
+        }
+
+        private Game()
+        {
+            ServerHandler = new ServerHandler("include url here");
+            GameStarted = false;
+            GameData = "";
+        }
+
+        public void SetScene(int KeyLength)
+        {
+
+        }
+
+        public static Game Instance = new Game();
+    }
+}
---
+++ BB84GameLib/BB84GameLib/Scene.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace BB84GameLib
+{
+    public enum GameMode : byte
+    {
+        Blank,
+        CharacterSelection,
+        BasisSelection,
+        KeepDisregard
+    }
+
+    public enum Player : byte
+    {
+        Alice,
+        Bob,
+        Eve
+    }
+
+    public class Scene
+    {
+        public byte Keylength { get; private set; }
+        public byte TurnNumber { get; private set; }
+
+        public GameMode GameMode { get; private set; }
+
+        public bool[] AliceBases { get; private set; }
+        public bool[] BobBases { get; private set; }
+        public bool[] EveBases { get; private set; }
+
+        public bool[] AliceQubits { get; private set; }
+        public bool[] BobQubits { get; private set; }
+        public bool[] EveQubits { get; private set; }
+
+
+        //bases selection update structure
+        //Turn Number, Player, Alice/Bob/Eve Bases
+        //Alice/Bob/Eve Qubits
+
+        public Scene(byte keylength)
+        {
+            Keylength = keylength;
+
+            AliceBases = new bool[keylength];
+            BobBases = new bool[keylength];
+            EveBases = new bool[keylength];
+
+            AliceQubits = new bool[keylength];
+            //set qubits
+            for (int i = 0; i < keylength; i++)
+            {
+                AliceQubits[i] = new Random().Next(0, 2) == 1;
+            }
+            BobQubits = new bool[keylength];
+            EveBases = new bool[keylength];
+        }
+
+        public void ParseUpdate(string update)
+        {
+            //alice bob eve is always the order
+            string[] splitUpdate = update.Split('.');
+            TurnNumber = byte.Parse(splitUpdate[0]);
+
+            GameMode = (GameMode)byte.Parse(splitUpdate[1]);
+
+        }
+
+        public string BuildUpdate()
+        {
+            //put together all information in the correct order and return
+            //an update parseable by the ParseUpdate functiom
+
+            //return
+            return "not implemented";
+        }
+    }
+}
